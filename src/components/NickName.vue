4f0933443a789b883792b06938f2d4ed3a74a202
Misako Kuroda <misako.kuroda@rakus-partners.co.jp>
2025-01-08 17:53:04 +0900

    Headerコンポーネント作成

---
+++ src/components/NickName.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  data() {
+    return {
+      nickname: "",
+    };
+  },
+  async mounted() {
+    const res = await fetch("http://localhost:3000/api/nickname/1")
+    const data = await res.json()
+    this.nickname = data.nickname
+  }
+};
+</script>
+
+<template>
+  <div class="nickname">
+    <p>ユーザーネーム</p>
+    <p>{{ nickname }}</p>
+  </div>
+</template>
+
+<style>
+.nickname {
+  position: absolute;
+  left: 250px;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/TodayGoal.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  data() {
+    return {
+      content: "",
+    };
+  },
+  async mounted() {
+    const res = await fetch("http://localhost:3000/api/goal/1")
+    const data = await res.json()
+    this.content = data.content
+  },
+};
+</script>
+
+<template>
+  <div class="today-goal">
+    <p>今日の目標：{{ content }}</p>
+  </div>
+</template>
+
+<style>
+.today-goal {
+  font-size: 24px;
+  font-weight: bold;
+  top: 20px;
+  position: absolute;
+  left: 450px;
+}
+</style>
