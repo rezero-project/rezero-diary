bbebae7dac61f7aa97bf5bfdc2a93dc4f1af082d
hayato.misawa <m.hayato.recruit@gmail.com>
2025-01-08 11:17:14 +0900

    #10 Footerの作成

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <footer class="footer">
+    <img src="" alt="リゼロ画像" class="footer_image">
+    <p class="footer_paragraph">©︎Re:ゼロから始める人生日記</p>
+  </footer>
+</template>
+
+<style>
+.footer {
+  display: flex;
+  align-items: center;
+  gap: 30px;
+  background-color: #DAEFD1;
+  width: 100%;
+  height: 10%;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+}
+
+.footer_image {
+  position: absolute;
+}
+
+.footer_paragraph {
+  margin: 0 auto;
+}
+</style>
