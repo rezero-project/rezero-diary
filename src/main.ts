86535208f239b46923bc9f172e174aa748d4d0de
hayato.misawa <m.hayato.recruit@gmail.com>
2025-01-17 17:34:13 +0900

    #27 DiaryNavコンポーネントの作成

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router/index.ts";
+import vuetify from "./plugins/vuetify";
 
 const app = createApp(App);
+app.use(vuetify);
 app.use(router);
 app.mount("#app");
---
+++ src/pages/NicknamePage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import NicknameForm from '../components/NicknameForm.vue';
+</script>
+
+<template>
+  <NicknameForm />
+</template>
+
+<style scoped></style>
